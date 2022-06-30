5bc465a1d7fc2c6241dca26668d54bb396eb1283
Farhan Khan <farhan.k2005@gmail.com>
2022-06-30 17:26:35 -0400

    created connection and added tab logo

---
+++ src/config/database.php
@@ -0,0 +1,14 @@
+<?php
+    //create constant variables for database connection
+    define('DB_HOST', 'localhost');
+    define('DB_USER', 'farhankhan');
+    define('DB_PASS', 'farhan12345');
+    define('DB_NAME', 'uni_managment_system');
+    
+    //create connection
+    $conn = mysqli_connect(DB_HOST, DB_USER, DB_PASS, DB_NAME);
+
+    //check if connection is established
+    if(!$conn){die('Connection failed: '.mysqli_connect_error());}
+?>
+<html><head><link rel="icon" type="image/png" href="../public/img/logo.png"/></head></html> <!----Tab logo ------>
