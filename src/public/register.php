493e464e67809df87203710450483842480a4e4a
Farhan Khan <farhan.k2005@gmail.com>
2022-07-10 19:37:19 -0400

    made registration work

--- src/public/register.php
+++ src/public/register.php
@@ -30,7 +30,52 @@
     <?php
       //prepared statement to add student to database
       $register_stmt = $conn->prepare("INSERT INTO students (first_name, last_name, dob, phone_number, address, email, student_pass) VALUES (?,?,?,?,?,?,?)");
-      $register_stmt->bind_param("sssssss", $fName, $lName, $dob, $phNum, $address, $email, $student_pass);
+      $register_stmt->bind_param("sssssss", $first_name, $last_name, $dob, $phNum, $address, $email, $student_pass);
+      $checkEmail_stmt = $conn->prepare("SELECT * FROM students WHERE email = ?");
+      $checkEmail_stmt->bind_param("s",$email);
+      //check if form is submitted
+      if (isset($_POST['register'])) {
+        //assign variables
+        $email = htmlspecialchars($_POST['email']);
+        $student_pass = htmlspecialchars($_POST['password']);
+        $confirm_password = htmlspecialchars($_POST['retypedPass']);
+        $first_name = htmlspecialchars($_POST['firstName']);
+        $last_name = htmlspecialchars($_POST['lastName']);
+        $address = htmlspecialchars($_POST['address']);
+        $phNum = htmlspecialchars($_POST['phoneNumber']);
+        $dob = htmlspecialchars($_POST['dob']);
+
+        //check if all fields are filled
+        if (
+          !empty($email) &&
+          !empty($student_pass) &&
+          !empty($confirm_password) &&
+          !empty($first_name) &&
+          !empty($last_name)
+        ) {
+          //check if passwords match
+          if ($student_pass == $confirm_password) {
+            //check if email is valid
+            if (filter_var($email, FILTER_VALIDATE_EMAIL)) {
+              $checkEmail_stmt -> execute();
+              $checkEmail_stmt -> store_result();
+              //check if email already exists
+              if ($checkEmail_stmt -> num_rows > 0) {
+                echo "<script>alert('Email already exists')</script>";
+              } else {
+                  //insert user into database
+                  $register_stmt -> execute();
+                  $register_stmt -> store_result();
+                  echo ('<script> alert("Registration Successful"); window.location.href = "login.php"; </script>');
+              }
+            } else {
+              echo "<script>alert('Invalid email')</script>";
+            }
+          } else {
+            echo "<script>alert('Passwords do not match')</script>";
+          }
+        }
+      }
     ?>
     <canvas id="canvas"></canvas>
     <div class="container">
@@ -38,7 +83,7 @@
         <img src="../public/img/bg.svg" />
       </div>
       <div class="login-content">
-        <form autocomplete="new-password">
+        <form autocomplete="off" action=<?php echo htmlspecialchars($_SERVER['PHP_SELF']); ?> method="POST" enctype="multipart/form-data">
           <img src="../public/img/loginAvatar.svg" />
           <h2 class="title">Student Registration Portal</h2>
           <div class="input-div one">
@@ -83,7 +128,7 @@
               <h5>Date of Birth</h5>
               <input
                 type="date"
-                name="DOB"
+                name="dob"
                 class="input date"
                 required
                 autocomplete="off"
@@ -99,7 +144,7 @@
             <div class="div">
               <h5>Address</h5>
               <input
-                type="email"
+                type="text"
                 name="address"
                 class="input"
                 required
@@ -157,6 +202,23 @@
                 autocomplete="off"
                 required
                 oninvalid="this.setCustomValidity('Please enter a valid password')"
+                oninput="setCustomValidity('')"
+              />
+            </div>
+          </div>
+          <div class="input-div pass">
+            <div class="i">
+              <i class="fas fa-lock"></i>
+            </div>
+            <div class="div">
+              <h5>Confirmed Password</h5>
+              <input
+                type="password"
+                name="retypedPass"
+                class="input"
+                autocomplete="off"
+                required
+                oninvalid="this.setCustomValidity('Please re-enter your password')"
                 oninput="setCustomValidity('')"
               />
             </div>
